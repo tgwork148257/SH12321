5fa40a60b389697b9873d74e965b62bbb4226408
Tommy Zhang <Tommy@tommys-macbook-pro.local>
2016-06-13 12:52:09 +0800

    modify the request file

---
+++ SH12321/Request/TGRequest.h
@@ -0,0 +1,14 @@
+//
+//  TGRequest.h
+//  SH12321
+//
+//  Created by Tommy on 6/13/16.
+//  Copyright © 2016 12321. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface TGRequest : NSObject
+
+@end
---
+++ SH12321/Request/TGRequest.m
@@ -0,0 +1,15 @@
+//
+//  TGRequest.m
+//  SH12321
+//
+//  Created by Tommy on 6/13/16.
+//  Copyright © 2016 12321. All rights reserved.
+//
+
+#import "TGRequest.h"
+#import "AFNetworking.h"
+
+@implementation TGRequest
+
+
+@end
