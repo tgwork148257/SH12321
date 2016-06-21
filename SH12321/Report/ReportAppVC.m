b5d7ffa01bc361016adb9265edd353ab1f2b5181
Tommy Zhang <Tommy@tommys-macbook-pro.local>
2016-06-21 16:55:45 +0800

    modify the report App vc

--- SH12321/Report/ReportAppVC.m
+++ SH12321/Report/ReportAppVC.m
@@ -8,16 +8,77 @@
 
 #import "ReportAppVC.h"
 
-@implementation ReportAppVC
+@implementation ReportAppVC{
+    
+    UIView *reportView;
+    ReportItemLabel *AppNameLabel;
+    ReportItemTextField *AppNameTextField;
+    
+    ReportItemLabel *AppSourceLabel;
+    ReportItemTextField *AppSourceTextField;
+    
+    ReportItemLabel *reportContentLabel;
+    ReportItemTextView *reportContentTextView;
+    
+    CommitButton *commitBtn;
+    
+    ReportDataModel *model;
+}
 
 - (void)viewDidLoad {
     self.navigationTitle = @"举报手机应用";
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    
+    self.view.backgroundColor = grayBgColor;
+    reportView = [[UIView alloc] init];
+    reportView.backgroundColor = C_WHITE;
+    model = [[ReportDataModel alloc] init];
+    model.reportType = ReportMessage;
+    
+    [self addSubviews];
+}
+
+- (void)addSubviews{
+    AppNameLabel = [ReportItemLabel initWithY:0 title:@"不良App名称" superView:reportView];
+    AppNameTextField = [ReportItemTextField initWithY:AppNameLabel.y + AppNameLabel.height placeholder:nil superView:reportView];
+    
+    AppSourceLabel = [ReportItemLabel initWithY:AppNameTextField.y + AppNameTextField.height title:@"不良App来源" superView:reportView];
+    AppSourceTextField = [ReportItemTextField initWithY:AppSourceLabel.y + AppSourceLabel.height placeholder:nil superView:reportView];
+    
+    reportContentLabel = [ReportItemLabel initWithY:AppSourceTextField.y + AppSourceTextField.height title:@"不良App描述" superView:reportView];
+    reportContentTextView = [ReportItemTextView initWithY:reportContentLabel.y + reportContentLabel.height placeholder:nil superView:reportView];
+    
+    reportView.frame = CGRectMake(0, ORIGIN_Y, DEVICE_W, reportContentTextView.y + reportContentTextView.height);
+    [self.view addSubview:reportView];
+    
+    commitBtn = [CommitButton initWithY:reportView.y + reportView.height + 20 superView:self.view];
+    [commitBtn addTarget:self action:@selector(commitReport) forControlEvents:UIControlEventTouchUpInside];
+    
 }
 
 - (void)commitReport{
     
+    if (![TGUtils isNumber:AppNameTextField.text]) {
+        [TGToast showWithText:@"发送号码输入错误"];
+        return;
+    }
+    
+    if (![TGUtils isNumber:AppSourceTextField.text]) {
+        [TGToast showWithText:@"接收号码输入错误"];
+        return;
+    }
+    
+    model.reportSendNumber = AppNameTextField.text;
+    model.reportAcceptNumber = AppSourceTextField.text;
+    model.reportContent = reportContentTextView.text;
+    [[TGService sharedInstance] commitReportWithData:model success:^(id responseObject) {
+        [TGToast showWithText:@"举报成功"];
+        [self.navigationController popViewControllerAnimated:YES];
+    } fail:^{
+        [TGToast showWithText:@"举报失败，请重试"];
+    }];
 }
+
 
 @end
